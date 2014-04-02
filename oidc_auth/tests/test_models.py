12a59263428ec043668f4f42669d3778cc9765ba
Lucas Sampaio <lucas@lsmagalhaes.com>
2014-04-02 18:03:41 -0300

    boilerplate code moved to helper

--- oidc_auth/tests/test_models.py
+++ oidc_auth/tests/test_models.py
@@ -2,39 +2,11 @@ from urlparse import urljoin
 import mock
 from nose import tools
 
+from .utils import OIDCTestCase
 from oidc_auth.models import OpenIDProvider
 
 
-class TestOpenIDPRovider(object):
-    def __init__(self):
-        self.issuer = 'http://example.it'
-        self.configs = {
-            'issuer': self.issuer,
-            'authorization_endpoint': urljoin(self.issuer, 'authorize'),
-            'token_endpoint': urljoin(self.issuer, 'token'),
-            'userinfo_endpoint': urljoin(self.issuer, 'userinfo_endpoint'),
-            'jwks_uri': urljoin(self.issuer, 'jwks_uri'),
-        }
-
-        # to be used with requests lib
-        self.response_mock = mock.MagicMock()
-        self.response_mock.status_code = 200
-        self.response_mock.json.return_value = self.configs
-    
-    def tearDown(self):
-        OpenIDProvider.objects.all().delete()
-    
-    def assert_provider_valid(self, provider, credentials=None):
-        if not credentials:
-            credentials = self.configs
-
-        tools.assert_is_instance(provider, OpenIDProvider)
-        tools.assert_equal(provider.issuer, credentials['issuer'])
-        tools.assert_equal(provider.authorization_endpoint, credentials['authorization_endpoint'])
-        tools.assert_equal(provider.token_endpoint, credentials['token_endpoint'])
-        tools.assert_equal(provider.userinfo_endpoint, credentials['userinfo_endpoint'])
-        tools.assert_equal(provider.jwks_uri, credentials['jwks_uri'])
-
+class TestOpenIDPRovider(OIDCTestCase):
     @mock.patch('requests.get')
     def test_discover_by_url(self, get_mock):
         get_mock.return_value = self.response_mock
