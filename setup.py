8ac0c9768f7afeaf50e77112f4d39e855d5ab524
Lucas Sampaio <lucas@lsmagalhaes.com>
2014-04-15 11:58:15 -0300

    setup.py created

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-oidc-auth',
+    version='0.1',
+    description='OpenID Connect client for Django applications',
+    long_description='WIP',
+    author='Lucas S. Magalhães',
+    author_email='lucas.sampaio@intelie.com.br',
+    packages=find_packages(exclude=['*.tests']),
+    include_package_data=True,
+    install_requires=[
+        'Django',
+        'South',
+        'pyjwkest',
+        'requests',
+    ],
+    zip_safe=True
+)
