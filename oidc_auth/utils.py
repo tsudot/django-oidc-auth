68b0ac768c5c6c22b2465e1f6f7043a704610df5
Lucas Sampaio <lucas@lsmagalhaes.com>
2014-04-15 14:07:09 -0300

    Suppress warning

--- oidc_auth/utils.py
+++ oidc_auth/utils.py
@@ -5,10 +5,10 @@ from .settings import oidc_settings
 
 
 def scopes():
-    scopes = set(oidc_settings.SCOPES)
-    scopes.update({'openid', 'profile', 'email'})
+    _scopes = set(oidc_settings.SCOPES)
+    _scopes.update({'openid', 'profile', 'email'})
 
-    return ' '.join(scopes)
+    return ' '.join(_scopes)
 
 
 def b64decode(token):
