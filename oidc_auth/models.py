dc9ee09d13ef704365e39a8c73b2c0baa8e25eac
Lucas Sampaio <lucas@lsmagalhaes.com>
2014-10-23 15:47:05 -0200

    get_or_create with __iexact lookup

--- oidc_auth/models.py
+++ oidc_auth/models.py
@@ -226,8 +226,11 @@ class OpenIDUser(models.Model):
 
         # Find an existing User locally or create a new one
         try:
-            user = UserModel.objects.get(username=id_token['sub'])
+            user = UserModel.objects.get(username__iexact=id_token['sub'])
             log.debug('Found user with username %s locally' % id_token['sub'])
+        except UserModel.MultipleObjectsReturned:
+            user = UserModel.objects.filter(username__iexact=id_token['sub'])[0]
+            log.warn('Multiple users found with username %s! First match will be selected' % id_token['sub'])
         except UserModel.DoesNotExist:
             log.debug('User with username %s not found locally, '
                       'so it will be created' % id_token['sub'])
